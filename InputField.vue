b3824accf685edd2fc8c545395f487e5f7e9cc84
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2024-11-27 18:42:07 -0300

    added ref to InputField

--- InputField.vue
+++ InputField.vue
@@ -1,7 +1,7 @@
 <template>
   <div :class="dense ? 'q-pa-xs' : 'q-pa-sm'">
     <!-- Input text: -->
-    <q-input hide-bottom-space v-if="type == 'text' || type == null"
+    <q-input ref="InputFieldRef" hide-bottom-space v-if="type == 'text' || type == null"
       :class="`full-width bg-${BgColor ? BgColor : 'white'}`" square filled :dense="dense" :clearable="clearable"
       :readonly="readonly" :disable="disable" v-model="value" :error="Error" @focus="() => $emit('focus')" type="text"
       :maxlength="maxlength" :label="Label" :mask="Mask" @update:model-value="updModelValue">
@@ -12,7 +12,7 @@
     </q-input>
 
     <!-- Input password: -->
-    <q-input hide-bottom-space v-if="type == 'password'" :class="`full-width bg-${BgColor ? BgColor : 'white'}`" square
+    <q-input ref="InputFieldRef" hide-bottom-space v-if="type == 'password'" :class="`full-width bg-${BgColor ? BgColor : 'white'}`" square
       filled :dense="dense" :clearable="clearable" :readonly="readonly" :disable="disable" v-model="value" :error="Error"
       @focus="() => $emit('focus')" type="password" :maxlength="maxlength" :label="Label"
       @update:model-value="updModelValue">
@@ -23,7 +23,7 @@
     </q-input>
 
     <!-- Input textarea: -->
-    <q-input hide-bottom-space v-if="type == 'textarea'" :class="`full-width bg-${BgColor ? BgColor : 'white'}`" square
+    <q-input ref="InputFieldRef" hide-bottom-space v-if="type == 'textarea'" :class="`full-width bg-${BgColor ? BgColor : 'white'}`" square
       filled :dense="dense" :clearable="clearable" :readonly="readonly" :disable="disable" v-model="value" :error="Error"
       @focus="() => $emit('focus')" type="textarea" :maxlength="maxlength" :label="Label"
       @update:model-value="updModelValue">
@@ -34,7 +34,7 @@
     </q-input>
 
     <!-- Input email: -->
-    <q-input hide-bottom-space v-if="type == 'email'" :class="`full-width bg-${BgColor ? BgColor : 'white'}`" square
+    <q-input ref="InputFieldRef" hide-bottom-space v-if="type == 'email'" :class="`full-width bg-${BgColor ? BgColor : 'white'}`" square
       filled :dense="dense" :clearable="clearable" :readonly="readonly" :disable="disable" v-model="value" :error="Error"
       @focus="() => $emit('focus')" type="email" maxlength="255" :label="Label" @update:model-value="updModelValue">
       <template v-slot:append>
@@ -44,7 +44,7 @@
     </q-input>
 
     <!-- Input number: -->
-    <q-input hide-bottom-space v-if="type == 'number'" :class="`full-width bg-${BgColor ? BgColor : 'white'}`" square
+    <q-input ref="InputFieldRef" hide-bottom-space v-if="type == 'number'" :class="`full-width bg-${BgColor ? BgColor : 'white'}`" square
       filled :dense="dense" :clearable="clearable" :readonly="readonly" :disable="disable" v-model="value" :error="Error"
       @focus="() => $emit('focus')" type="number" :step="step" :max="max" :min="min" :label="Label"
       @update:model-value="updModelValue">
