5e5d48d57ac69e93c2b64c74c06da97b47e884ee
Mkopaes <murilo.paes191@gmail.com>
2024-12-09 16:18:49 -0300

    Update: Input Editor Toolbar

--- InputEditor.vue
+++ InputEditor.vue
@@ -3,9 +3,54 @@
     <label v-if="Label" for="editor" class="q-my-md">
       <i class="fas fa-caret-right"></i> {{ Label }}
     </label>
-    <q-editor v-model="value" :disable="disable" :readonly="readonly" :placeholder="placeholder"
-      :class="`full-width ${this.Error ? 'error' : ''}`"
-      @update:model-value="$emit('focus')">
+    <q-editor v-model="value" :disable="disable" :readonly="readonly" :placeholder="placeholder" 
+      :class="`full-width ${this.Error ? 'error' : ''}`" @focus="() => $emit('focus')"
+      :toolbar="[
+        ['left', 'center', 'right', 'justify'],
+        ['bold', 'italic', 'strike', 'underline', 'subscript', 'superscript'],
+        ['hr', 'link'],
+        ['print', 'fullscreen'],
+        [
+          {
+            label: $q.lang.editor.fontSize,
+            icon: $q.iconSet.editor.fontSize,
+            fixedLabel: true,
+            fixedIcon: true,
+            list: 'no-icons',
+            options: ['size-1','size-2','size-3','size-4','size-5','size-6','size-7']
+          },
+          {
+            label: $q.lang.editor.defaultFont,
+            icon: $q.iconSet.editor.font,
+            fixedIcon: true,
+            list: 'no-icons',
+            options: [
+              'default_font',
+              'arial',
+              'arial_black',
+              'comic_sans',
+              'courier_new',
+              'impact',
+              'lucida_grande',
+              'times_new_roman',
+              'verdana'
+            ]
+          },
+          'removeFormat'
+        ],
+        ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
+        ['undo', 'redo'],
+      ]"
+      :fonts="{
+        arial: 'Arial',
+        arial_black: 'Arial Black',
+        comic_sans: 'Comic Sans MS',
+        courier_new: 'Courier New',
+        impact: 'Impact',
+        lucida_grande: 'Lucida Grande',
+        times_new_roman: 'Times New Roman',
+        verdana: 'Verdana'
+      }">
     </q-editor>
   </div>
 </template>
